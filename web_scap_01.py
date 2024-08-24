5c647a63991cd4d9a643615376cb23d6b902b332
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-08-24 15:25:08 +0530

    Update web_scap_01.py

--- web_scap_01.py
+++ web_scap_01.py
@@ -18,12 +18,12 @@ from transformers import T5ForConditionalGeneration, T5Tokenizer
 ### Title and description for your Streamlit app
 #---------------------------------------------------------------------------------------------------------------------------------
 #import custom_style()
-st.set_page_config(page_title="Web Scrapper",
+st.set_page_config(page_title="Web Scrapper | v0.3",
                    layout="wide",
                    #page_icon=               
                    initial_sidebar_state="collapsed")
 #----------------------------------------
-st.title(f""":rainbow[Web Scrapper | v0.3]""")
+st.title(f""":rainbow[Web Scrapper]""")
 st.markdown('Created by | <a href="mailto:avijit.mba18@gmail.com">Avijit Chakraborty</a>', 
             unsafe_allow_html=True)
 st.info('**Disclaimer : :blue[Thank you for visiting the app] | Unauthorized uses or copying of the app is strictly prohibited | Click the :blue[sidebar] to follow the instructions to start the applications.**', icon="ℹ️")
