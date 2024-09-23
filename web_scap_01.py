e8fae3882d75282419bec27757077542a9e13fe1
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-09-23 11:20:02 +0530

    Update web_scap_01.py

--- web_scap_01.py
+++ web_scap_01.py
@@ -16,8 +16,8 @@ col1.write('test')
 col2.button('Click me')
 with st.sidebar.popover('Popover with columns (WORKING)'):
         col1, col2 = st.columns(2)
-            col1.write('INSIDE THE POPOVER')
-            col2.button('DO SOMETHING')
+        col1.write('INSIDE THE POPOVER')
+        col2.button('DO SOMETHING')
 
 with col3.popover('Popover with columns (NOT WORKING)'):
             col1, col2 = st.columns(2)
